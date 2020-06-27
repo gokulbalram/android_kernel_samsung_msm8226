16d9e3321a71c4c2feccbd51f2a17a42f2874630
Gokul Balram <gokulbalram@gmail.com>
2020-06-27 11:59:15 +0530

    Fix cherry-pick mishap

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -631,6 +631,11 @@ static void cpufreq_interactive_timer(unsigned long data)
 	 * floor frequency for the minimum sample time since last validated.
 	 */
 
+	if (pcpu->minfreq_boost) {
+	 	min_sample_time = 0;
+	 	pcpu->minfreq_boost = 0;
+ 	}
+
 	if (new_freq < pcpu->floor_freq) {
 		if (now - pcpu->floor_validate_time < min_sample_time) {
 			trace_cpufreq_interactive_notyet(
